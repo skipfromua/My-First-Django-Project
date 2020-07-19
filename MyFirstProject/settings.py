a4629fa623d6b150a74302aaac8ab8b0acf14a32
skipfromua <skipfromua@gmail.com>
2020-07-19 03:41:46 +0300

    'Deplo'

--- MyFirstProject/settings.py
+++ MyFirstProject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,7 +85,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
---
+++ Procfile
@@ -0,0 +1,1 @@
+web MyFirstProject.wsgi --log-file -
