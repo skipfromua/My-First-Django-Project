54a7f3edcec608174086fe50bc1f8ece251b764b
skipfromua <skipfromua@gmail.com>
2020-07-19 04:06:12 +0300

    'Deplo'

--- MyFirstProject/wsgi.py
+++ MyFirstProject/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import django
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstProject.settings')
 
 application = get_wsgi_application()
-application = django(application)
+application = WhiteNoise(application)
